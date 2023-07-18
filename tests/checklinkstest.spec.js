4ffa4580dc3234c099034d1fa1a19efe2d552e8d
Pranav Pothiwala <pothiwalapranav@gmail.com>
2023-07-18 15:39:42 +0100

    Update import to latest ES6 module

--- tests/checklinkstest.spec.js
+++ tests/checklinkstest.spec.js
@@ -1,4 +1,4 @@
-const { test, expect } = require('@playwright/test');
+import { test, expect } from '@playwright/test';
 
 test('Check links on the page @checklinks', async ({ page }) => {
     await page.goto('');
